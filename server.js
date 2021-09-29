f4c23903e7a52b006882122e32302f5633025091
Dominic Tremblay <dominictremblay24@gmail.com>
2021-09-29 13:09:09 -0400

    final demo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const uuid = require('uuid/v4');
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,6 +19,9 @@ app.use(express.static('public'));
 
 // Setting ejs as the template engine
 app.set('view engine', 'ejs');
+
+// activate cookieParser
+app.use(cookieParser());
 
 // In memory database
 const movieQuotesDb = {
@@ -81,8 +85,131 @@ const updateQuote = (quoteId, content) => {
   return true;
 };
 
+// AUTHENTICATION HELPER FUNCTIONS
+
+const findUserByEmail = function (email, users) {
+  for (let userId in users) {
+    const user = users[userId];
+    if (email === user.email) {
+      return user;
+    }
+  }
+
+  return false;
+};
+
+const createUser = function (name, email, password, users) {
+  const userId = uuid().substring(0, 6);
+
+  // adding to an object
+  // objectname[key] = value
+  // Create a new user
+  users[userId] = {
+    id: userId,
+    name,
+    email,
+    password,
+  };
+
+  return userId;
+};
+
+const authenticateUser = function (email, password, usersDb) {
+  // retrieve the user from the db
+  const userFound = findUserByEmail(email, usersDb);
+
+  // compare the passwords
+  // password match => log in
+  // password dont' match => error message
+  if (userFound && userFound.password === password) {
+    return userFound;
+  }
+
+  return false;
+};
+
 app.get('/', (req, res) => {
   res.redirect('/quotes');
+});
+
+// AUTHENTICATION ROUTES (login + register)
+
+app.get('/register', (req, res) => {
+  const templateVars = { user: null };
+
+  // display the register form
+  res.render('register', templateVars);
+});
+
+app.post('/register', (req, res) => {
+  // we need to extract the info from the body of request => req.body
+  console.log('req.body:', req.body);
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // const {name, email, password} = req.body; // destructuring
+
+  // check if that user already exist in the usersDb
+  // if yes, send back error message
+  // if no, we're good
+
+  // userFound can be a user object OR
+  // false
+  const userFound = findUserByEmail(email, usersDb);
+
+  console.log('userFound:', userFound);
+
+  if (userFound) {
+    res.status(401).send('Sorry, that user already exists!');
+    return;
+  }
+
+  // userFound is false => ok register the user
+
+  const userId = createUser(name, email, password, usersDb);
+
+  // Log the user => ask the browser to set a cookie with the user id
+  res.cookie('user_id', userId);
+
+  // redirect to '/quotes'
+
+  res.redirect('/quotes');
+});
+
+app.get('/login', (req, res) => {
+  // if we here, we take for granted that the user is not logged in.
+  const templateVars = { user: null };
+
+  res.render('login', templateVars);
+});
+
+app.post('/login', (req, res) => {
+  // extract the email and password from the body of request => req.body
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+
+  // compare the passwords
+  // password match => log in
+  // password dont' match => error message
+
+  const user = authenticateUser(email, password, usersDb);
+
+  if (user) {
+    // user is authenticated
+    // setting the cookie
+    res.cookie('user_id', user.id);
+
+    // redirect to /quotes
+    res.redirect('/quotes'); //=> hey browser, can you do another request => get /quotes
+    return;
+  }
+
+  // user is not authenticated => send error
+
+  res.status(401).send('Wrong credentials!');
 });
 
 // CRUD operations
@@ -93,19 +220,53 @@ app.get('/', (req, res) => {
 
 app.get('/quotes', (req, res) => {
   const quoteList = Object.values(movieQuotesDb);
-  const templateVars = { quotesArr: quoteList };
+
+  // retrieve the user info => email? => cookies to rerieve the user id
+  const userId = req.cookies['user_id'];
+
+  console.log({ userId });
+
+  // retrieving the user object from usersDb
+  const loggedInUser = usersDb[userId];
+
+  const templateVars = { quotesArr: quoteList, user: loggedInUser };
 
   res.render('quotes', templateVars);
 
   // res.json(movieQuotesDb);
 });
 
+
+
 // Display the add quote form
 // READ
 // GET /quotes/new
 
 app.get('/quotes/new', (req, res) => {
-  res.render('new_quote');
+  // retrieve the user info => email? => cookies to rerieve the user id
+  const userId = req.cookies['user_id'];
+
+  // retrieving the user object from usersDb
+  const loggedInUser = usersDb[userId];
+
+  const templateVars = { user: loggedInUser };
+
+  res.render('new_quote', templateVars);
+});
+
+// Display the form
+// GET /quotes/:id
+app.get('/quotes/:id', (req, res) => {
+  const quoteId = req.params.id;
+  // retrieve the user info => email? => cookies to rerieve the user id
+  const userId = req.cookies['user_id'];
+  // retrieving the user object from usersDb
+  const loggedInUser = usersDb[userId];
+
+  const templateVars = { quoteObj: movieQuotesDb[quoteId], user: loggedInUser };
+
+  // render the show page
+  res.render('quote_show', templateVars);
 });
 
 // Add a new quote
@@ -128,15 +289,7 @@ app.post('/quotes', (req, res) => {
 
 // Edit a quote
 
-// Display the form
-// GET /quotes/:id
-app.get('/quotes/:id', (req, res) => {
-  const quoteId = req.params.id;
-  const templateVars = { quoteObj: movieQuotesDb[quoteId] };
 
-  // render the show page
-  res.render('quote_show', templateVars);
-});
 
 // Update the quote in the movieQuotesDb
 // PUT /quotes/:id
