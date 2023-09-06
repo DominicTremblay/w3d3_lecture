17b9e9fa48489f1819d1a8f6567ab2d14e796f31
Dominic Tremblay <dominictremblay24@gmail.com>
2023-09-06 15:13:41 -0400

    final version

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const uuid = require('uuid/v4');
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,7 +11,10 @@ const app = express();
 // morgan middleware allows to log the request in the terminal
 app.use(morgan('short'));
 
-// parse application/x-www-form-urlencoded
+// cookie-parser
+app.use(cookieParser());
+
+// parse application/x-www-form-urlencoded => body parser
 app.use(express.urlencoded({ extended: false }));
 
 // Static assets (images, css files) are being served from the public folder
@@ -97,6 +101,15 @@ const usersDb = {
   },
 };
 
+// Helpers Functions
+
+// return the user object it finds with the email provided from users db or return null
+const findUser = email => {
+// todo
+
+
+}
+
 const createNewQuote = (content) => {
   const quoteId = uuid().substr(0, 8);
 
@@ -124,6 +137,105 @@ app.get('/', (req, res) => {
   res.redirect('/quotes');
 });
 
+app.get('/users.json', (req, res) => {
+  res.json(usersDb);
+});
+
+// User Authentication Routes
+
+// Register Routes
+app.get('/register', (req, res) => {
+  // Display the register form
+  const templateVars = { user: null };
+
+  res.render('register', templateVars);
+});
+
+app.post('/register', (req, res) => {
+  // extract the info from the form => bodyParser
+  const { name, email, password } = req.body;
+
+  // const name = req.body.name;
+  // const email = req.body.email;
+  // const password = req.body.password;
+
+  // validation => check if the user is already in the db (email)
+  // if exists => send back an error message
+
+  for (let userId in usersDb) {
+    console.log('user id:', userId);
+    console.log('user info (value):', usersDb[userId]);
+    // const user = usersDb[userId];
+    if (email === usersDb[userId].email) {
+      res.status(403);
+      return res.send('403 - User already exists');
+    }
+  }
+
+  // if not => add the new user the the usersDb\
+  // create a new user and add to users db
+
+  // syntax add a new key/value pair in an object?
+  // objectName[new key] = value
+  const userId = uuid().substring(0, 8); // ex.: 2e8f0150
+
+  usersDb[userId] = {
+    id: userId,
+    name,
+    email,
+    password,
+  };
+
+  // sign in the user
+  // set the user id in the cookie
+  res.cookie('user_id', userId);
+
+  // redirect /quotes
+
+  res.redirect('/quotes');
+});
+
+// Login Routes
+
+app.get('/login', (req, res) => {
+  // Display the login form
+  const templateVars = { user: null };
+
+  res.render('login', templateVars);
+});
+
+app.post('/login', (req, res) => {
+  // extract the form info
+  const { email, password } = req.body;
+
+  // authenticate the user
+  // check if the user exist (email)=>
+  // if yes => check the password => sign the user in (cookie)
+  // if not => error message
+
+  for (let userId in usersDb) {
+    if (
+      email === usersDb[userId].email &&
+      password === usersDb[userId].password
+    ) {
+      // user is authenticated
+      // set the cookie
+      res.cookie('user_id', userId);
+      // redirect to /quotes
+      return res.redirect('/quotes');
+    }
+  }
+  res.status(401);
+  res.send('Wrong Credentials');
+});
+
+// Logout
+app.post('/logout', (req, res) => {
+  // clear the cookies
+  res.clearCookie('user_id');
+  res.redirect('/quotes');
+});
+
 // CRUD operations
 
 // List all the quotes
@@ -132,7 +244,11 @@ app.get('/', (req, res) => {
 
 app.get('/quotes', (req, res) => {
   const quoteList = Object.values(movieQuotesDb);
-  const templateVars = { quotesArr: quoteList };
+
+  // retrieve our user
+  const loggedInUser = usersDb[req.cookies['user_id']];
+
+  const templateVars = { quotesArr: quoteList, user: loggedInUser };
 
   res.render('quotes', templateVars);
 
@@ -171,7 +287,9 @@ app.post('/quotes', (req, res) => {
 // GET /quotes/:id
 app.get('/quotes/:id', (req, res) => {
   const quoteId = req.params.id;
-  const templateVars = { quoteObj: movieQuotesDb[quoteId] };
+  // retrieve the loggedIn user
+  const loggedInUser = usersDb[req.cookies['user_id']];
+  const templateVars = { quoteObj: movieQuotesDb[quoteId], user: loggedInUser };
 
   // render the show page
   res.render('quote_show', templateVars);
